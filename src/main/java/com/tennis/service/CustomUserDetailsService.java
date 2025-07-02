30b4095db78bea6303244075739c61e119b7af57
Stefan Radomirović <radomirovicstefan5@gmail.com>
2025-07-02 15:49:02 +0200

    Improve CustomUserDetailsService readability and add Lombok

--- src/main/java/com/tennis/service/CustomUserDetailsService.java
+++ src/main/java/com/tennis/service/CustomUserDetailsService.java
@@ -1,29 +1,27 @@
 package com.tennis.service;
 
-import java.util.Set;
-import java.util.stream.Collectors;
+import com.tennis.model.User;
+import com.tennis.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.*;
 import org.springframework.stereotype.Service;
-import com.tennis.model.User;
-import com.tennis.repository.UserRepository;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @Primary
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
 	private final UserRepository userRepository;
 
-	public CustomUserDetailsService(UserRepository userRepository) {
-		this.userRepository = userRepository;
-	}
-
 	@Override
 	public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
-		User user = userRepository.findByEmail(usernameOrEmail).orElseGet(
-				() -> userRepository.findByUsername(usernameOrEmail).orElseThrow(() -> new UsernameNotFoundException(
-						"User not found with email or username: " + usernameOrEmail)));
+		User user = userRepository.findByEmail(usernameOrEmail).or(() -> userRepository.findByUsername(usernameOrEmail))
+				.orElseThrow(() -> new UsernameNotFoundException(
+						"User not found with email or username: " + usernameOrEmail));
 
 		Set<SimpleGrantedAuthority> authorities = user.getRoles().stream()
 				.map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName())).collect(Collectors.toSet());
