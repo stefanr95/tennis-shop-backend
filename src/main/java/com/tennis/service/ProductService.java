bc42cddc154f78160e264deb94d0d7947a8a3b89
Stefan Radomirović <radomirovicstefan5@gmail.com>
2025-07-02 15:54:31 +0200

    Clean up ProductService and add Lombok

--- src/main/java/com/tennis/service/ProductService.java
+++ src/main/java/com/tennis/service/ProductService.java
@@ -1,27 +1,22 @@
 package com.tennis.service;
 
+import com.tennis.model.Product;
+import com.tennis.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import com.tennis.model.Product;
-import com.tennis.repository.ProductRepository;
 
 @Service
+@RequiredArgsConstructor
 public class ProductService {
 
 	private final ProductRepository productRepository;
 
-	// Konstruktor za ProductRepository
-	public ProductService(ProductRepository productRepository) {
-		this.productRepository = productRepository;
-	}
-
-	// Dodavanje novog proizvoda
 	public Product addProduct(Product product) {
 		return productRepository.save(product);
 	}
 
-	// Izmena postojećeg proizvoda
 	public Product updateProduct(Long id, Product updatedProduct) {
 		Product product = productRepository.findById(id).orElseThrow(() -> new RuntimeException("Product not found."));
 
@@ -34,12 +29,10 @@ public class ProductService {
 		return productRepository.save(product);
 	}
 
-	// Dohvatanje svih proizvoda
 	public Page<Product> getAllProducts(Pageable pageable) {
 		return productRepository.findAll(pageable);
 	}
 
-	// Brisanje proizvoda
 	public void deleteProduct(Long id) {
 		productRepository.deleteById(id);
 	}
